5d8bd06221f9d881d676cb5a225dab5f75e354e4
zakrmaks <zakrmaks@gmail.com>
2020-07-15 19:51:34 +0300

    Homework

--- src/HomeWork_18/Main.java
+++ src/HomeWork_18/Main.java
@@ -1,12 +1,9 @@
 package HomeWork_18;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -21,7 +18,6 @@ Gson gson = new Gson();
         for (int i = 0; i < numOfItter; i++) {
             student.add(new Student("Ivanov "+(int)Math.abs(Math.random()*range+4),"Ivan "+(int)Math.abs(Math.random()*range+4), (int)Math.abs(Math.random()*range+4), rand.nextBoolean()));
         }
-//        System.out.println(student.toString());
         String JSON = gson.toJson(student);
         System.out.println(JSON);
         FileWriter writer = new FileWriter("D:\\hillel\\Homeworks\\src\\HomeWork_18\\student.json");
