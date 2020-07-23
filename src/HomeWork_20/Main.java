16dc4d17c4a627a9b8918470752e234c9591c82a
zakrm <zakrmaks@gmail.com>
2020-07-23 10:44:46 +0300

    Find Summ

--- src/HomeWork_20/Main.java
+++ src/HomeWork_20/Main.java
@@ -1,22 +1,26 @@
 package HomeWork_20;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[]{3,5, 7,9,15, 16,20};
-int num = 16;
-int left = 0;
-int right = arr.length-1;
-while (left!=right){
-    int summ = arr[left] + arr[right];
-    if (summ>num){
-        right--;
-    }else if(summ<num){
-        left++;
-    }else {
-        System.out.println(left+" "+ right);
-        return;
-    }
-}
-    }
+        int[] arr = new int[]{3, 5, 7, 9, 15, 16, 20};
+        HashMap array = new HashMap();
+        for (int i = 0; i < arr.length; i++) {
+            array.put(arr[i],i);
+        }
+        int num1 = 0;
+        Object num2 = null;
+        int num = 20;
+        for (int i = 0; i < arr.length; i++) {
+            int dimens = num - arr[i];
+            if (array.containsKey(dimens) == true){
+                num1 = i;
+                num2 = array.get(dimens);
+            }
+        }
+        System.out.println(num1 +"   "+num2.toString());
 
+    }
 }
+
